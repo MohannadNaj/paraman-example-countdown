a6431de3fe3dc311ce03242f01f3e6e0676df345
MohannadNaj <me@mohannad.io>
2017-12-11 11:12:08 +0300

    Integrating countdown theme into laravel

---
+++ resources/assets/js/countdown.js
@@ -0,0 +1,8 @@
+window.$ = window.jQuery = require('jquery');
+
+require('bootstrap-sass');
+
+require("./countdown/jquery.cookie.js");
+require("./countdown/jquery.countdown.min.js");
+require("./countdown/front.js");
+
